6e4d2aab66e79bfc7111eefd2ed6669d46789924
Rjviviers <Rjviviers@gmail.com>
2020-10-29 12:44:51 +0200

    profile pic workin

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+
 // use App\Http\Controllers\Auth\
 class ProfilesController extends Controller
 {
@@ -12,24 +14,41 @@ class ProfilesController extends Controller
         $this->middleware('auth');
     }
 
-    public function index(\App\Models\User $user)
+    public function index(User $user)
     {
         return view('profiles.index', compact('user'));
     }
 
-    public function edit(\App\Models\User $user)
+    public function edit(User $user)
     {
+        $this->authorize('update', $user->profile);
         return view('profiles.edit', compact('user'));
     }
 
-    public function update(User $user){
+    public function update(User $user)
+    {
+        $this->authorize('update', $user->profile);
         $data = request()->validate([
             'title'=>'required',
             'description'=> 'required',
             'url'=>'url',
             'img'=>'image',
         ]);
-        auth()->user()->profile->update($data);
+
+        if (request('img')) {
+            $imagePath = request('img')->store('profile', 'public');
+            // php intervention package
+            // this is to crop images to square
+            $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000, 1000);
+            $image->save();
+            auth()->user()->profile->update(array_merge(
+                $data,
+                ['img'=>$imagePath]
+            ));
+        } else {
+            auth()->user()->profile->update($data);
+        }
+
         return redirect("/profile/{$user->id }");
     }
 }
--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -7,12 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
-
     use HasFactory;
     protected $guarded = [];
+    public function profileImage()
+    {
+        $imagePath = ($this->img) ?  $this->img : 'profile/0ob9hF4Nz8rlbY5U9VoKoEgubMgBjQcP9KwN2LdD.png';
+        return '/storage/' . $imagePath ;
+    }
     public function user()
     {
         return $this->belongsTo(User::class);
     }
-
 }
