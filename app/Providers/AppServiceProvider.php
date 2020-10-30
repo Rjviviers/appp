f0480e3c38a2ab25fcfdaf4b802c8dfcbc636ba9
Rjviviers <Rjviviers@gmail.com>
2020-10-30 09:56:44 +0200

    db setup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        //fix for pagination making weird oversize
         Paginator::useBootstrap();
     }
 }
